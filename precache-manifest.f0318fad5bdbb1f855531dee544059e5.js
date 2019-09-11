a52e48fc700e95ab621cd490575124c9cf2613af
George701 <chernikov.georgii@gmail.com>
2019-09-11 16:01:06 +0300

    Updates

---
+++ precache-manifest.f0318fad5bdbb1f855531dee544059e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c478bb356ca221534c157b7a110873c",
+    "url": "/sliding-cards/index.html"
+  },
+  {
+    "revision": "837d12db6cd7064cfe3e",
+    "url": "/sliding-cards/static/css/main.9e898920.chunk.css"
+  },
+  {
+    "revision": "19fadd3cbe711bbfeea2",
+    "url": "/sliding-cards/static/js/2.1f9707a2.chunk.js"
+  },
+  {
+    "revision": "837d12db6cd7064cfe3e",
+    "url": "/sliding-cards/static/js/main.0380cfd8.chunk.js"
+  },
+  {
+    "revision": "a3f34ab2f025041744a2",
+    "url": "/sliding-cards/static/js/runtime~main.7aee7c6d.js"
+  }
+]);
